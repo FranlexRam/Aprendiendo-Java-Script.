3326e22b840cc65c4df9d5fdcba2a25df69cecf8
Franlex Ramirez <franlex.ramirez@gmail.com>
2022-10-15 08:20:23 -0400

    Agregar dos ejemplos de funciones declarativas

--- funciones.js
+++ funciones.js
@@ -1,4 +1,4 @@
-// Funciones Declarativas: No es anonima ya que se le esta colocando un nombre.
+// ***Funciones Declarativas: No es anonima ya que se le esta colocando un nombre.
 
 function miFuncion() {
     return 3;
@@ -7,7 +7,7 @@ function miFuncion() {
 miFuncion (); //Se manda a llamar la funcion declarativa con el nombre de la funcion.
 
 
-// Funciones de expresion: Se le conoce como funciones anonimas porque la funcion no tiene nombre.
+// ***Funciones de expresion: Se le conoce como funciones anonimas porque la funcion no tiene nombre.
 
 var miFuncion = function (a,b) {
     return a + b;
@@ -15,3 +15,22 @@ var miFuncion = function (a,b) {
 
 miFuncion(); // Se manda a llamar la funcion expresiva con el nombre de la variable.
 // Al ser una funcion, se colocan los parentesis.
+
+
+
+//Funciones declarativas usando console.log:
+
+function SaludarEstudiantes (estudiante) {
+    console.log(`Hola ${estudiante}`);
+}
+SaludarEstudiantes ("Diego"); //Llamar la funcion.
+
+
+//Funciones declarativas usando return:
+
+function sumar (a,b) {
+    var resultado = a + b;
+    return resultado;
+}
+
+sumar (1,2); //Llamar la funcion.
