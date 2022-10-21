32222cf16e423b087fc4654c10036a6b24ec07d8
Franlex Ramirez <franlex.ramirez@gmail.com>
2022-10-20 22:37:05 -0400

    operadores

---
+++ operadores.js
@@ -0,0 +1,26 @@
+//Operadores aritmeticos
+
+// Suma
+2 + 3 // 5
+// Resta
+5 - 3 // 2
+// Multiplicación
+4 * 2 // 8
+// División
+6 / 2 // 3
+//Residuo
+21 % 5 // 1
+//Concatenacion
+"Hola " + "Platzi" // "Hola Platzi"
+
+//operador de incremento y decremento
+var a = 3
+var b = 3
+
+console.log(a++) //3
+console.log(++b) //4
+console.log(a) //4
+console.log(b) //4
+
+
+
