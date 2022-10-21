ddb4b60fce8798d119847570f71a1d7cf8486920
Franlex Ramirez <franlex.ramirez@gmail.com>
2022-10-21 06:45:54 -0400

    actualizacion de operadores

--- operadores.js
+++ operadores.js
@@ -22,5 +22,46 @@ console.log(++b) //4
 console.log(a) //4
 console.log(b) //4
 
+//otro ejemplo:
+var edad = 40;
+edad++;
+edad //41
+edad+=2;//43
+
+//ejemplo:
+var contador = 1
+
+contador += 2
+contador -= 1
+contador *= 5
+contador /= 2
+
+console.log(contador)//5
+
+
+
+//operadores de mayor o menor:
+
+// Menor que
+3 < 5 // true
+
+// Mayor que
+3 > 5 // false
+
+// Mayor o igual a que
+3 >= 3 // true
+3 >= 5 // false
+
+// Menor o igual a que
+3 <= 3 // true
+3 <= 5 // true
+
+
+//Disyuncion logica:
+var a = 15
+var b = 5
+
+(a >= 10) && (a <= 20) // true
+(b >= 10) && (b <= 20) // false
 
 
