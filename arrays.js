c546371cfc809040bd1b08a5c5c25c1352c167b2
Franlex Ramirez <franlex.ramirez@gmail.com>
2022-10-25 22:54:34 -0400

    Metodos mutables

---
+++ arrays.js
@@ -0,0 +1,22 @@
+//Para conocer la cantidad de elementos dentro de un array:
+
+var array = [1,2,3,4,5,6];
+var longitud = array.length;
+console.log(longitud);//6
+
+
+var frutas = ["Manzana","Platano", "Cereza","Fresa"];
+console.log(frutas);//(4) ["Manzana","Platano", "Cereza","Fresa"]
+
+//Para acceder a un elemento especifico:
+console.log(frutas[0]);//Manzana
+console.log(frutas [2]);//Cereza
+
+//Para agregar mas elementos al final del array:
+var frutas = ["Manzana","Platano", "Cereza","Fresa"];
+var masFrutas = frutas.push("Uvas");
+console.log(frutas);
+
+//Para quitar ultimo elemento:
+var ultimo = frutas.pop("Uvas");
+console.log(frutas);
