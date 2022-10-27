3c9d9c5462ee66d7fa60425b22338d26c0115bab
Franlex Ramirez <franlex.ramirez@gmail.com>
2022-10-27 19:03:16 -0400

    Acomodando el codigo

--- while.js
+++ while.js
@@ -1,6 +1,12 @@
+/*Tenemos un programa únicamente para estudiantes, queremos controlar que un usuario ingrese una edad menor de 18
+(y mayor que 11 porque es la edad minima para ingresar (no existen edades negativas).
+Entonces utilizamos while debido a que no conocemos cuántas veces se equivocará el usuario.*/
+
+
+
 var edad = Number(prompt("Ingresar tu edad: "));
 
-while (edad >18 || edad <=0){
+while (edad >18 || edad <11){
     edad = Number(prompt("Ingrese un valor correcto: "))
 }
 
