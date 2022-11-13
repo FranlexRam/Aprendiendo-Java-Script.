6816df1243031c45556a021f801efe1a2c664a22
Franlex Ramirez <franlex.ramirez@gmail.com>
2022-11-13 19:59:35 -0400

    Constructor function learning

--- while.js
+++ while.js
@@ -1,5 +1,5 @@
-/*Tenemos un programa únicamente para estudiantes, queremos controlar que un usuario ingrese una edad menor de 18
-(y mayor que 11 porque es la edad minima para ingresar (no existen edades negativas).
+/*Tenemos un programa únicamente para estudiantes hasta 18 anhos y queremos controlar que un usuario ingrese una edad menor de 18
+y mayor que 11 porque es la edad minima para ingresar (no existen edades negativas).
 Entonces utilizamos while debido a que no conocemos cuántas veces se equivocará el usuario.*/
 
 
@@ -7,7 +7,7 @@ Entonces utilizamos while debido a que no conocemos cuántas veces se equivocar�
 var edad = Number(prompt("Ingresar tu edad: "));
 
 while (edad >18 || edad <11){
-    edad = Number(prompt("Ingrese un valor correcto: "))
+    edad = Number(prompt("Ingrese un valor correcto: ")) //WHILE va a colocar el rango de valores que sera excluido.
 }
 
 console.log("Aceptado, gracias por ingresar al programa");
